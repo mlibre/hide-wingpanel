66b668fad977c782dbee389c0ad033a1fea637fe
capstan1 <capstan@gmx.de>
2017-09-29 16:14:00 +0200

    Update to enable toggle

--- show_hide_top_panel.bash
+++ show_hide_top_panel.bash
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-printf "#!/bin/bash\nkillall cerbere &> /dev/null\nwait\nkillall wingpanel &> /dev/null" > /usr/bin/hide_top_panel
+printf "#!/bin/bash\nresult=`ps aux | grep -i "wingpanel" | grep -v "grep" | wc -l`\nif [ $result -ge 1 ]\nthen\nkillall cerbere &> /dev/null\nwait\nkillall wingpanel &> /dev/null\nelse\nwingpanel &> /dev/null &\nfi > /usr/bin/hide_top_panel
 wait
 chmod +x /usr/bin/hide_top_panel
-wait
-
-printf "#!/bin/bash\nwingpanel &> /dev/null &" > /usr/bin/show_top_panel
-wait
-chmod +x /usr/bin/show_top_panel
