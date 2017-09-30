88fea6a533f2b9c4102b10c4627ec5253cfa514a
capstan1 <capstan@gmx.de>
2017-09-30 07:29:07 +0200

    Add files via upload

--- show_hide_top_panel.bash
+++ show_hide_top_panel.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-printf "#!/bin/bash\nresult=`ps aux | grep -i "wingpanel" | grep -v "grep" | wc -l`\nif [ $result -ge 1 ]\nthen\nkillall cerbere &> /dev/null\nwait\nkillall wingpanel &> /dev/null\nelse\nwingpanel &> /dev/null &\nfi > /usr/bin/hide_top_panel
+cp ./hide_top_panel /usr/bin/hide_top_panel
 wait
 chmod +x /usr/bin/hide_top_panel
